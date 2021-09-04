b1055b59bc2b384c5a0acc50ad275bf98d2b80ee
saurabhs <saurabhpshirsath@gmail.com>
2021-09-04 20:54:03 +0530

    [saurabh]Add:Retrive only ProductId From Product Review Management

--- ProductReviewManagement/Management.cs
+++ ProductReviewManagement/Management.cs
@@ -58,12 +58,24 @@ namespace ProductReviewManagement
                 Console.WriteLine("Product Id : " + item.ProductID + "\t\tProduct Rating : " + item.Rating);
             }
         }
+        public void SelectIdAndReview(List<ProductReview> review)
+        {
+            var recordedData = from list in review
+                               select new { list.ProductID, list.Review };
+           
+                                
+            foreach (var item in recordedData)
+            {
+                Console.WriteLine("Product Id : " + item.ProductID + "\t\tProduct Review : " + item.Review);
+            }
+        
+        }
         public void Operations()
         {
             Program program = new Program();
             while (choice !=10)
             {
-                Console.WriteLine("\n Enter 1 for Display top Three records\n Enter 2 for Display Selected records\n Enter 3 for Display Count of Records For ProductID\n Enter 4 for Display ProductId And Review \n Enter 5 for Skip Top Five Records \n Enter 6 for exit ");
+                Console.WriteLine("\n Enter 1 for Display top Three records\n Enter 2 for Display Selected records\n Enter 3 for Display Count of Records For ProductID\n Enter 4 for Display ProductId And Review \n Enter 5 for Skip Top Five Records \n Enter 6 for Display ProductId And Review  \n Enter 7 for exit ");
                 choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -81,6 +93,9 @@ namespace ProductReviewManagement
                         break;
                     case 5:
                         SkipTopFiveRecords(Program.List);
+                        break;
+                    case 6:
+                        SelectIdAndReview(Program.List);
                         break;
                     default:
                         Console.WriteLine("Entered wrong input");
