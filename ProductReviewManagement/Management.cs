274689c8d6909d764c219ad9188bff5c335f7fc7
saurabhs <saurabhpshirsath@gmail.com>
2021-09-04 13:46:56 +0530

    [saurabh]Add:Retrive Count Of Each ProductID

--- ProductReviewManagement/Management.cs
+++ ProductReviewManagement/Management.cs
@@ -31,13 +31,21 @@ namespace ProductReviewManagement
                 Console.WriteLine("Product Id: " + item.ProductID + "\tUser Id: " + item.UserID + "\tProduct Rating: " + item.Rating + "\tProduct Review:" + item.Review + "\tIs Like: " + item.IsLike);
             }
         }
+        public void RetriveCountOfREcords(List<ProductReview> review)
+        {
+            var recordedData = review.GroupBy(x => x.ProductID).Select(x => new { ProductID = x.Key, Count = x.Count() });
 
+            foreach (var item in recordedData)
+            {
+                Console.WriteLine(item.ProductID + "---" + item.Count);
+            }
+        }
         public void Operations()
         {
             Program program = new Program();
             while (choice !=10)
             {
-                Console.WriteLine("\n Enter 1 for Display top Three records\n Enter 2 for Display Selected records\n Enter 3 for exit ");
+                Console.WriteLine("\n Enter 1 for Display top Three records\n Enter 2 for Display Selected records\n Enter 3 for Display Count of Records For ProductID\n Enter 4 for exit ");
                 choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -46,6 +54,9 @@ namespace ProductReviewManagement
                         break;
                     case 2:
                         RetrivePerticularRecord(Program.List);
+                        break;
+                    case 3:
+                        RetriveCountOfREcords(Program.List);
                         break;
 
                     default:
